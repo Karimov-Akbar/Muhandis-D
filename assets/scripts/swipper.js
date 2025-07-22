111433f0d89b1ad5ccd7a9fc382a79a05f3abed4
Karimov-Akbar <akbarkarimov250@gmail.com>
2025-07-22 18:39:03 +0500

    first commit

---
+++ assets/scripts/swipper.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    lazy: true,
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
+
+const swiperThumbs = new Swiper(".topInnovationsSwiper-thumbs", {
+    loop: false,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+
+const swiperMain = new Swiper(".topInnovationsSwiper-main", {
+    lazy: true,
+    loop: true,
+    spaceBetween: 10,
+    thumbs: {
+        swiper: swiperThumbs,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
